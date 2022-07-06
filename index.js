40e13e0231e8274aa7fad937c5b70f73fa923708
Mithu Kumar Gupta <guptamithu2019@gmail.com>
2022-07-06 12:52:37 +0530

    randomquotesextension

---
+++ index.js
@@ -0,0 +1,20 @@
+const getjokes = async() => {
+    const abc = await fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "quotes15.p.rapidapi.com",
+            "x-rapidapi-key": '3e76d9a4e2msh6353427a523c5dbp1d7fc3jsn5c2eec0d2042',
+        }
+    })
+    .then((response) => response.json())
+    .then(response => {
+        document.getElementById('quote').innerHTML = response.content; 
+        document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+window.addEventListener("load",()=>{
+    getjokes();
+})
